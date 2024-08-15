35537eec5ef487352c019963172e31735bb6af9b
codingkunst <ryoo6931@gmail.com>
2024-08-15 16:58:18 +0900

    [FEAT] 불필요 코드 삭제

--- src/components/PrmcCard.jsx
+++ src/components/PrmcCard.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import ModalDetail from "./ModalDetail";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { toggleFavoritePfmc } from "../api/user";
 
-const PrmcCard = ({ item, toggleFavoritePfmcHandler, fill }) => {
+const PrmcCard = ({ item }) => {
   // 모달 관련 코드
   const [show, setShow] = useState(false);
 
@@ -19,23 +18,6 @@ const PrmcCard = ({ item, toggleFavoritePfmcHandler, fill }) => {
         <Card.Body className="d-grid gap-2">
           <Card.Title>공연명 : {item.prfnm}</Card.Title>
           <Card.Text>{item.prfstate}</Card.Text>
-          <button onClick={toggleFavoritePfmcHandler}>
-            좋아요 추가
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill={fill}
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </button>
           <Button variant="secondary" onClick={handleShow}>
             자세히 보기
           </Button>
