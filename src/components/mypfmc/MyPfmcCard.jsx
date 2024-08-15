d16074a34ad4d2af5f3ffc3fd162e97202f99534
codingkunst <ryoo6931@gmail.com>
2024-08-15 19:52:40 +0900

    [FIX] 병합 오류 수정

--- src/components/mypfmc/MyPfmcCard.jsx
+++ src/components/mypfmc/MyPfmcCard.jsx
@@ -6,12 +6,13 @@ import Card from "react-bootstrap/Card";
 const MyPfmcCard = ({ item }) => {
   return (
     <div className="m-2.5">
-    <Card style={{ width: "18rem" }}>
-      <Card.Img className="w-auto h-auto" variant="top" src={item.poster} alt="Loading..." />
-      <Card.Body className="d-grid gap-2">
-        <Card.Title>공연명 : {item.prfnm}</Card.Title>
-      </Card.Body>
-    </Card></div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img className="w-auto h-auto" variant="top" src={item.poster} alt="Loading..."/>
+        <Card.Body className="d-grid gap-2">
+          <Card.Title>공연명 : {item.prfnm}</Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
