d3718ea9d112aa286463e90081d76110b8095b59
noeulgim <noeul3240@gmail.com>
2024-08-15 11:20:57 +0900

    [Fix] auth update

--- src/zustand/useAuthStore.js
+++ src/zustand/useAuthStore.js
@@ -2,50 +2,40 @@ import { create } from "zustand";
 import Cookies from "js-cookie";
 
 const useAuthStore = create((set) => ({
-  AccessToken: Cookies.get("AccessToken") || null,
-  token: localStorage.getItem("refreshToken") || null,
+  token: Cookies.get("refreshToken") || null,
+  accessToken: localStorage.getItem("accessToken") || null,
   isAuthenticated: Cookies.get("refreshToken") ? true : false,
-  login: (AccessToken, token) => {
+  login: (accessToken, refreshToken) => {
     // 리프레시 토큰은 쿠키에 저장
-    if (AccessToken || token) {
-      Cookies.set("AccessToken", AccessToken);
-      // 액세스 토큰은 로컬 스토리지에 저장 (새로고침 시 토큰 유지)
-      localStorage.setItem("refreshToken", token);
+    Cookies.set("refreshToken", refreshToken);
+    // 액세스 토큰은 로컬 스토리지에 저장 (새로고침 시 토큰 유지)
+    localStorage.setItem("accessToken", accessToken);
 
-      set({
-        token: token,
-        AccessToken,
-        isAuthenticated: true,
-      });
+    set({
+      token: refreshToken,
+      accessToken,
+      isAuthenticated: true,
+    });
 
-      console.log("Refresh Token saved:", localStorage.getItem("refreshToken"));
-      console.log("Access Token saved in Cookies:", Cookies.get("AccessToken"));
-      console.log(isAuthenticated);
-    }
+    console.log("Access Token saved:", localStorage.getItem("accessToken"));
+    console.log("Refresh Token saved in Cookies:", Cookies.get("refreshToken"));
   },
   logout: () => {
-    Cookies.remove("AccessToken");
-    localStorage.removeItem("refreshToken");
+    Cookies.remove("refreshToken");
+    localStorage.removeItem("accessToken");
 
     set({
       token: null,
-      AccessToken: null,
+      accessToken: null,
       isAuthenticated: false,
     });
 
-    console.log("Refresh Token removed:", localStorage.getItem("refreshToken"));
+    console.log("Access Token removed:", localStorage.getItem("accessToken"));
     console.log(
-      "Access Token removed from Cookies:",
-      Cookies.get("AccessToken")
+      "Refresh Token removed from Cookies:",
+      Cookies.get("refreshToken")
     );
     window.location.reload();
-  },
-  initialize: () => {
-    set({
-      AccessToken: Cookies.get("AccessToken") || null,
-      token: localStorage.getItem("refreshToken") || null,
-      isAuthenticated: Cookies.get("AccessToken") ? true : false,
-    });
   },
 }));
 
